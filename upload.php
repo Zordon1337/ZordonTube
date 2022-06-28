8eaa48af5a9d01ecd96fb75b629c8c7c91f7938b
Zordon <65111609+Zordon1337@users.noreply.github.com>
2022-06-28 16:30:04 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -21,6 +21,22 @@
     </style>
 </head>
 <body style="background-color: black;">
+<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+    <a class="navbar-brand" href="home">ZordonTube</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="videos.php">Videos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="upload.php">Upload</a>
+            </li>
+        </ul>
+    </div>
+</nav>
 <form action="uploadprocess.php" method='post' enctype="multipart/form-data">
 <input style="color: white;" type="file" name="file"/><br><br>
 	<input class="btn btn-outline-secondary" type="submit" value="Upload"/>
